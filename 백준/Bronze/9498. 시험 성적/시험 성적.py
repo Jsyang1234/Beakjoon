74dc540e2ce1fa62ece20b8522c2c765e749119e
Jsyang1234 <Southpole370@gmail.com>
2024-11-26 18:39:56 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+if n >= 90 and n <= 100:
+    print("A")
+if n >= 80 and n <= 89:
+    print("B")
+if n >= 70 and n <= 79:
+    print("C")
+if n >= 60 and n <= 69:
+    print("D")
+if n <= 59:
+    print("F")
